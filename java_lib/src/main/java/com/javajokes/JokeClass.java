7174083540207ed1301141100bc5005d0b60aaf2
Alexandra Perdomo <aperdomo@mahisoft.com>
2016-04-04 16:06:39 -0600

    java library added

---
+++ java_lib/src/main/java/com/javajokes/JokeClass.java
@@ -0,0 +1,17 @@
+package com.javajokes;
+
+import java.util.Random;
+
+public class JokeClass {
+    public static String[] jokes = {
+            "what do you do with epileptic lettuce?, You make a seizure salad!",
+            "Why did the police officer smell?, Because he was on duty",
+            "Why did the skeleton go to the party alone?, He had no body to go with him!"};
+
+    public static String getJoke(){
+        Random rand = new Random();
+
+        int  n = rand.nextInt(3);
+        return jokes[n];
+    }
+}
